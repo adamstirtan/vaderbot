296cd77cc4ef48f8448d66c4048dde8571e86df7
Adam Stirtan <adamstirtan@gmail.com>
2016-02-11 20:32:20 -0500

    Bot connects and shows reads dumps messages to output

---
+++ vader.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import time
+from slackclient import SlackClient
+
+
+class Vader:
+    client = None
+    token = "xoxb-16470487171-NEqcYbtwqYrDWeXktwbWVUho"
+
+    def __init__(self):
+        self.client = SlackClient(self.token)
+
+    def connect(self):
+        self.client.rtm_connect()
+        while True:
+            events = self.client.rtm_read()
+            for event in events:
+                self.process_event(event)
+            time.sleep(1)
+
+    def process_event(self, event):
+        if event["type"] == "message":
+            print(event["text"])
+
+Vader().connect()
