8317f878cf66f084dbca776300834c9576725f72
Adam Stirtan <adamstirtan@gmail.com>
2016-02-12 14:22:06 +0000

    Added exception handling

--- vader.py
+++ vader.py
@@ -5,11 +5,13 @@ from slackclient import SlackClient
 
 
 class Vader:
+
     client = None
     token = "xoxb-16470487171-NEqcYbtwqYrDWeXktwbWVUho"
 
     def __init__(self):
         self.client = SlackClient(self.token)
+
 
     def connect(self):
         self.client.rtm_connect()
@@ -19,8 +21,13 @@ class Vader:
                 self.process_event(event)
             time.sleep(1)
 
+
     def process_event(self, event):
-        if event["type"] == "message":
-            print(event["text"])
+        try:
+            if event["type"] == "message":
+                print(event["text"])
+        except KeyError:
+            pass
+
 
 Vader().connect()
