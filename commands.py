894143327ac4237e77092b6e97936333cf52d91f
Adam Stirtan <adamstirtan@gmail.com>
2016-02-12 10:55:15 -0500

    whoops

--- commands.py
+++ commands.py
@@ -11,7 +11,7 @@ def urban_dictionary(client, channel_name, query):
     if data is not None and data["list"] is not None and len(data["list"]) > 0:
         link = data["list"][0]["permalink"]
         try:
-            channel = next(channgit el for channel in client.server.channels if channel.name == channel_name)
+            channel = next(channel for channel in client.server.channels if channel.name == channel_name)
         except StopIteration:
             raise Exception("Could not find that channel")
         channel.send_message(link)
