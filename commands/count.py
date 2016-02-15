f5d84f254806fcac8867a22114042cbb5e8d6c92
Adam Stirtan <adamstirtan@gmail.com>
2016-02-14 21:52:43 -0500

    Added !count quotes

--- commands/count.py
+++ commands/count.py
@@ -8,6 +8,9 @@ def count(database, channel, params):
     if query == "messages":
         result = __count_messages__(database)
         channel.send_message("There are {} messages in my database.".format(result))
+    elif query == "quotes":
+        result = __count_quotes__(database)
+        channel.send_message("There are {} quotes in my database.".format(result))
 
 
 def __count_messages__(database):
@@ -15,6 +18,16 @@ def __count_messages__(database):
 
     cursor = db.cursor()
     result = cursor.execute('''SELECT count(id) FROM messages''').fetchone()
+
+    db.close()
+
+    return int(result[0])
+
+def __count_quotes__(database):
+    db = database.open()
+
+    cursor = db.cursor()
+    result = cursor.execute('''SELECT count(id) FROM quotes''').fetchone()
 
     db.close()
 
