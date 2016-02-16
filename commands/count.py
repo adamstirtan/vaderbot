6ec5747a13530749c66ef94479887451352cf386
Adam Stirtan <adamstirtan@gmail.com>
2016-02-15 22:00:20 -0500

    moving count logic to db client

--- commands/count.py
+++ commands/count.py
@@ -5,30 +5,7 @@ def count(database, channel, params):
 
     query = " ".join(params)
 
-    if query == "messages":
-        result = __count_messages__(database)
-        channel.send_message("There are {} messages in my database.".format(result))
-    elif query == "quotes":
-        result = __count_quotes__(database)
-        channel.send_message("There are {} quotes in my database.".format(result))
-
-
-def __count_messages__(database):
-    db = database.open()
-
-    cursor = db.cursor()
-    result = cursor.execute('''SELECT count(id) FROM messages''').fetchone()
-
-    db.close()
-
-    return int(result[0])
-
-def __count_quotes__(database):
-    db = database.open()
-
-    cursor = db.cursor()
-    result = cursor.execute('''SELECT count(id) FROM quotes''').fetchone()
-
-    db.close()
-
-    return int(result[0])
+    if query == "messages" or query == "quotes":
+        channel.send_message("There are {} {} in my database.".format(database.count(query), query))
+    else:
+        channel.send_message("Usage: !count [messages or quotes]")
--- database/databaseclient.py
+++ database/databaseclient.py
@@ -10,6 +10,7 @@ class DatabaseClient:
 
     def __initialize_tables__(self):
         db = self.open()
+
         cursor = db.cursor()
 
         cursor.execute('''CREATE TABLE IF NOT EXISTS messages (
@@ -29,3 +30,16 @@ class DatabaseClient:
 
     def open(self):
         return sqlite3.connect(self.db_file, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+
+    def count(self, table):
+        connection = None
+
+        try:
+            connection = self.open()
+            cursor = connection.cursor()
+            result = cursor.execute("SELECT COUNT(id) FROM {}".format(table)).fetchone()
+        finally:
+            if connection:
+                connection.close()
+
+        return int(result[0])
