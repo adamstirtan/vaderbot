f88990fd6cb3977e13071d37bab8cb6e0eb1e159
mastadonn <mastadonn>
2016-02-19 10:25:49 -0500

    Weather working
    Weather trigger working Woot

--- commands/weather.py
+++ commands/weather.py
@@ -1,6 +1,7 @@
 def weather(database, channel, params):
     import requests
     import json
+    import math
 
     if len(params) == 0:
         return
@@ -12,15 +13,10 @@ def weather(database, channel, params):
     response = requests.get("http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(query,api_key))
     data = json.loads(response.text)
 
-    description = data['weather'][0]['description']
-    # temp = data['temp']
-    name = data['name']
+    weather = "{} {} {}{} {}".format(data['name'],math.trunc(data['main']['temp']-273.15), "\N{DEGREE SIGN}","C " , data['weather'][0]['description'])
 
-    print(data)
-    print("-----------------------------")
-    print(description)
-    # print(temp)
-    print(name)
-    # print("-----------------------------")
-    # print (name,temp,description)
-    # channel.send_message(name,temp,description)
+    print(weather)
+
+    channel.send_message(weather)
+
+# weather(None,None,"burlington")
