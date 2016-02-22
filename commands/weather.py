c23a0ed1bd0a712fc946710fa7f029eb8c81d507
Michael Lawson <michael@mikelawson.ca>
2016-02-22 21:32:18 +0000

    checking http response code to prevent HTTP 429 too many requests

--- commands/weather.py
+++ commands/weather.py
@@ -10,9 +10,12 @@ def weather(database, channel, params):
     api_key = "44db6a862fba0b067b1930da0d769e98"
 
     response = requests.get("http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(query,api_key))
-    data = json.loads(response.text)
 
-    weather = "{} {} {}{} {}".format(data['name'],math.trunc(data['main']['temp']-273.15), "\N{DEGREE SIGN}","C " , data['weather'][0]['description'])
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        weather = "{} {}\N{DEGREE SIGN}C {}".format(data['name'],math.trunc(data['main']['temp']-273.15), data['weather'][0]['description'])
+    else:
+        weather = "I don't know!"
 
     channel.send_message(weather)
 
