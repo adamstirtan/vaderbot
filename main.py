60699051bef89cef6c8942a45e23960b3b938ff8
Adam Stirtan <adamstirtan@gmail.com>
2016-02-24 14:34:51 -0500

    Starting web front end

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3.5
 
 from vader import Vader
+from web.request_handler import BotRequestHandler
+from http.server import HTTPServer
 
 
 def main():
+    server_address = ("127.0.0.1", 80)
+    server = HTTPServer(server_address, BotRequestHandler)
+    server.serve_forever()
+
     bot = Vader()
     bot.connect()
 
