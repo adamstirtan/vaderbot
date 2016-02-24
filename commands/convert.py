868b94f4eec3e4d389d988e4de146f77e080b626
Michael Lawson <michael@mikelawson.ca>
2016-02-24 17:55:50 +0000

    stupid pi parsing and extra error check on casting amount to float

--- commands/convert.py
+++ commands/convert.py
@@ -1,11 +1,19 @@
 import json
 import requests
+import math
 
 def convert(database, channel, params):
     if len(params) == 3:
         source = params[0]
         dest = params[1]
-        amount = float(params[2])
+
+        try:
+            amount = float(params[2])
+        except ValueError as e:
+            if params[2] == 'π':
+                amount = math.pi
+            else:
+                amount = 0.0
 
         response = requests.get("http://api.fixer.io/latest?base={}&symbols={}".format(source,dest))
 
@@ -22,6 +30,3 @@ def convert(database, channel, params):
         channel.send_message(message)
     else:
         channel.send_message("Usage: !convert [currency from] [currency to] [amount]")
-
-if __name__ == "__main__":
-    convert(None,None,["EUR","CAD","1"])
