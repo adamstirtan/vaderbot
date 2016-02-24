f206bb07aaa3f42d497323c263557ed80f78d36e
Adam Stirtan <adamstirtan@gmail.com>
2016-02-24 14:11:17 -0500

    Revert "Added update command"
    This reverts commit 4ce17e1893c85ae5748084ee99f6dafc84af9cab.

--- commands/update.py
+++
@@ -1,9 +0,0 @@
-from subprocess import call
-
-
-def update(database, channel, params):
-    if len(params) != 0:
-        channel.send_message("Usage: !update")
-        return
-
-    call("systemctl restart vaderbot")
