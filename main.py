308677b821f69b554bc29012b26bc23b6d753305
Michael Lawson <michael@mikelawson.ca>
2016-02-25 02:11:10 +0000

    listen on all addresses

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from threading import Thread
 
 
 def main():
-    server_address = ("127.0.0.1", 80)
+    server_address = ("0.0.0.0", 80)
     server = HTTPServer(server_address, BotRequestHandler)
     server_thread = Thread(target = server.serve_forever)
     server_thread.daemon = True
