44adabbcf80e83c75c463c6289b92cc879506a27
Michael Lawson <michael@mikelawson.ca>
2016-02-25 02:02:56 +0000

    running web server in a thread

--- main.py
+++ main.py
@@ -3,12 +3,15 @@
 from vader import Vader
 from web.request_handler import BotRequestHandler
 from http.server import HTTPServer
+from threading import Thread
 
 
 def main():
-    #server_address = ("127.0.0.1", 80)
-    #server = HTTPServer(server_address, BotRequestHandler)
-    #server.serve_forever()
+    server_address = ("127.0.0.1", 80)
+    server = HTTPServer(server_address, BotRequestHandler)
+    server_thread = Thread(target = server.serve_forever)
+    server_thread.daemon = True
+    server_thread.start()
 
     bot = Vader()
     bot.connect()
