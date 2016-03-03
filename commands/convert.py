19afdaf6d06f571d4b0cb582080d07db6d54c5fb
Michael Lawson <michael@mikelawson.ca>
2016-03-03 00:13:19 +0000

    migrate to validate, create_message

--- commands/convert.py
+++ commands/convert.py
@@ -2,39 +2,59 @@ import json
 import requests
 import math
 
-from commands.command import Command
+try:
+    from commands.command import Command
+except ImportError:
+    from command import Command
 
 
 class ConvertCommand(Command):
 
     def __init__(self):
         Command.__init__(self)
+        self._request_uri = "http://api.fixer.io/latest?base={}&symbols={}"
+
+    def validate(self, parameters):
+        if len(parameters) == 3:
+            return True
+        return False
+
+    def create_output(self, parameters):
+        src, dst, amount = parameters
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            if parameters[2] == 'π':
+                amount = math.pi
+            else:
+                return ":("
+
+        return self._get_conversion(src, dst, amount)
 
     def execute(self, channel, parameters):
-        if len(parameters) == 3:
-            source = parameters[0]
-            dest = parameters[1]
-
-            try:
-                amount = float(parameters[2])
-            except ValueError:
-                if parameters[2] == 'π':
-                    amount = math.pi
-                else:
-                    amount = 0.0
-
-            response = requests.get("http://api.fixer.io/latest?base={}&symbols={}".format(source, dest))
-
-            info = json.loads(response.content.decode("utf8"))
-
-            if not "error" in info and "rates" in info:
-                if dest in info["rates"]:
-                    message = "{:.2f}{} = {:.2f}{}".format(amount, source, info["rates"][dest] * amount, dest)
-                else:
-                    message = "Something isn't right."
-            else:
-                message = "Something isn't right."
-
-            channel.send_message(message)
+        if self.validate(parameters):
+            message = self.create_output(parameters)
         else:
-            channel.send_message("Usage: !convert [currency from] [currency to] [amount]")
+            message = "Usage: !convert [source currency] [destination currency] [amount]"
+
+        channel.send_message(message)
+
+    def _get_conversion(self, src, dst, amount):
+        response = requests.get(self._request_uri.format(src, dst))
+        info = json.loads(response.text)
+
+        if not "error" in info and "rates" in info and dst in info["rates"]:
+            message = "{:.2f}{} = {:.2f}{}".format(amount, src, info["rates"][dst] * amount, dst)
+        else:
+            message = "You done goofed."
+
+        return message
+
+if __name__ == "__main__":
+    from fakechannel import FakeChannel
+
+    channel = FakeChannel()
+
+    converter = ConvertCommand()
+    converter.execute(channel,["USD","CAD","1"])
