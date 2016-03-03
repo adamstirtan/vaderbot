37d546167f3de59684498f035d7df6314e7699bf
Adam Stirtan <adamstirtan@gmail.com>
2016-03-02 19:39:51 -0500

    Use @abstractmethod instead of @abstractclassmethod

--- commands/command.py
+++ commands/command.py
@@ -1,10 +1,13 @@
-from abc import ABCMeta, abstractclassmethod
+from abc import ABCMeta, abstractmethod
 
 
 class Command:
 
     __metaclass__ = ABCMeta
 
-    @abstractclassmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
     def execute(self, channel, parameters):
         pass
