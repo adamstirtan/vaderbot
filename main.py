14add995148097bfcead07982adcb5c53e805360
Adam Stirtan <adamstirtan@gmail.com>
2016-03-17 19:51:12 -0400

    Cleanup of potential crashes that we'll never get around to

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3.5
 
 from vader import Vader
-from web.request_handler import BotRequestHandler
-from http.server import HTTPServer
-from threading import Thread
 
 
 def main():
-    server = HTTPServer(("0.0.0.0", 80), BotRequestHandler)
-    server_thread = Thread(target=server.serve_forever)
-    server_thread.daemon = True
-    server_thread.start()
-
     bot = Vader()
     bot.connect()
 
