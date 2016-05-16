e5b376824399d3b6f8550d75fcb6b03d90bb9ab7
Michael Lawson <michael@mikelawson.ca>
2016-05-16 19:37:53 +0000

    debug print statement in weather command

--- commands/weather.py
+++ commands/weather.py
@@ -13,20 +13,32 @@ class WeatherCommand(Command):
         self._request_uri = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
         self._api_key = "44db6a862fba0b067b1930da0d769e98"
 
+    def validate(self, parameters):
+        if len(parameters) == 0:
+            return False
+        return True
+
+    def create_output(self, parameters):
+        pass
+
     def execute(self, channel, parameters):
         if len(parameters) == 0:
             return
 
         query = " ".join(parameters)
 
-        response = requests.get(self._request_uri.format(query, self._api_key))
-
-        if response.status_code == 200:
-            data = json.loads(response.text)
-            weather = "{} {}\N{DEGREE SIGN}C {}".format(data['name'],
-                                                        trunc(data['main']['temp'] - 273.15),
-                                                        data['weather'][0]['description'])
-        else:
+        try:
+            response = requests.get(self._request_uri.format(query, self._api_key))
+            print(response.text)
+            if response.status_code == 200:
+                data = json.loads(response.text)
+                weather = "{} {}\N{DEGREE SIGN}C {}".format(data['name'],
+                                                            trunc(data['main']['temp'] - 273.15),
+                                                            data['weather'][0]['description'])
+            else:
+                weather = "I don't know!"
+        except Exception as e:
+            print(e)
             weather = "I don't know!"
 
         channel.send_message(weather)
